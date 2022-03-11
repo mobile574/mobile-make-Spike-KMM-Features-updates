66bd05f2b74a3cf0b542b6949ecfc56539d7de8e
mobile574 <Cecillester721@proton.me>
2022-03-11 15:34:37 +0800

    iOS add big integer

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,9 +3,10 @@ import shared
 
 struct ContentView: View {
 	let greet = Greeting().greeting()
+    let sum = KFetcher().addOne(i: BignumBigInteger.init(long: INT64_MAX))
 
 	var body: some View {
-		Text(greet)
+        Text(greet + "\nINT64_MAX + 1 = " + sum.toString(base: 10))
 	}
 }
 
